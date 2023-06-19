1249b2b15d9767a477911ef7369f355e68eaea27
Lpik97 <Lpik97@users.noreply.github.com>
2023-06-19 09:16:15 +0200

    feat(GameAction modular function)
    A modular function that contains two functions, start and restart game that will be used to determine wether the game started or finished.

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const restartBtn = document.querySelector('.restart-btn');
 let gameStarted = false;
 let gameEnded = false;
 
-
 const playerFactory = (name) => {
     return name.toString();
 };
+
+const gameActions = (() => {
+    function startGame (event) {
+        event.preventDefault();
+        gameStarted = true;
+        form.style.display= 'none';
+        let playerName = playerFactory(document.getElementById('player-name').value);
+        let opponentName = playerFactory(document.getElementById('opponent-name').value);
+        restartBtn.style.display='initial';
+        return { playerName, opponentName };
+    };
+    function restartGame () {
+        cells.forEach((cell) => {
+            cell.innerHTML = '';
+        });
+        choices.totalChoices = [];
+        restartBtn.style.display='none';
+        form.style.display='initial';
+    };
+    return {startGame, restartGame };
+})();
