83589282d8b940eb8cf2cf19a1f8ccaec0eac1c6
Lpik97 <Lpik97@users.noreply.github.com>
2023-06-19 09:13:05 +0200

    feat(cells, form, restarBtn, gameStarted, gameEnded)
    Three elements were selected from the html file. And the game state variables were defined with a value of false.

---
+++ script.js
@@ -0,0 +1,5 @@
+const cells = document.querySelectorAll('.cell');
+const form = document.querySelector('.names-form');
+const restartBtn = document.querySelector('.restart-btn');
+let gameStarted = false;
+let gameEnded = false;
