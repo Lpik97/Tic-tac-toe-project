ede53622ead8b93a2576ec114c4fba1083c13ba1
Lpik97 <Lpik97@users.noreply.github.com>
2023-06-19 09:14:10 +0200

    feat(playerFactory factory function)
    A factory function named playerFactory was declared that takes a parameter of name and later returns it converted to a string.

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const form = document.querySelector('.names-form');
 const restartBtn = document.querySelector('.restart-btn');
 let gameStarted = false;
 let gameEnded = false;
+
+
+const playerFactory = (name) => {
+    return name.toString();
+};
